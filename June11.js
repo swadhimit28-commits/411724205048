a0662e0b3784c024acf39f00d1d15561118b4fde
swadhi <swadhi.m.it.28@psvpec.in>
2026-06-18 11:48:45 +0530

    JS

---
+++ June11.js
@@ -0,0 +1,43 @@
+console.log("Hi")
+const a = 10;
+console.log(a)
+const b = "String";
+console.log(b)
+
+const arr = [1, 2.1, "something", false]
+console.log(arr)
+
+arr[1] = 3.1
+console.log(arr[1])
+
+arr.push(4)
+console.log(arr)
+
+arr.pop()
+console.log(arr)
+
+for(let i=0; i <arr.length; i++){
+    example.log
+}
+
+function example() {
+    console.log("I am writing something")
+
+}
+
+const first = document.getElementById("ptag")
+first.innerHTML = "Paragraph"
+
+const input = document.getElementById("input")
+const output = document.getElementById("inputField")
+
+input.addEventListener("input", () => {
+    output.textContent = input.value
+})
+
+
+const ptag = document.createElement("p")
+ptag.textContent = "some text"
+document.body.appendChild(ptag)
+
+ptag.remove()
